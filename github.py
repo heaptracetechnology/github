f54d16d055028b889e53ce2dd198dfa26ed72028
Steve Peak <steve@codecov.io>
2018-03-14 10:32:35 +0100

    Update github.py

--- github.py
+++ github.py
@@ -6,19 +6,26 @@ import json
 import requests
 
 
-def main(command, query):
+def main(command, query, body):
     headers = {
         'Authorization': 'token %s' % os.getenv('API_TOKEN'),
         'User-Agent': os.getenv('USER_AGENT')
     }
+
     if command == 'get':
         uri = "https://api.github.com%s" % (query)
-        r = requests.get(uri, headers=headers)
-        return r.text
+        res = requests.get(uri, headers=headers)
+
+    if command == 'post':
+        uri = "https://api.github.com%s" % (query)
+        res = requests.post(uri, headers=headers, data=body)
 
     if command == 'graphql':
-        r = requests.post('https://api.github.com/graphql', json.dumps({"query": query}), headers=headers)
-        return r.text
+        uri = 'https://api.github.com/graphql'
+        res = requests.post(uri, json.dumps({"query": query}), headers=headers)
+
+    res.raise_for_status()
+    return r.text
 
 if __name__ == '__main__':
-    print(main(sys.argv[1], sys.argv[2]))
+    print(main(sys.argv[1], sys.argv[2], sys.argv[3]))
