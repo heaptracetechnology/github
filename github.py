9c9a872ccf3046f745d55fbf23d674f840c7556e
Steve Peak <steve@codecov.io>
2018-03-14 10:27:11 +0100

    return text format and skip json parsing

--- github.py
+++ github.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python
+
 import os
 import sys
 import requests
-import json
+
 
 def main(command, query):
     if command == 'get':
         uri = "https://api.github.com%s" % (query)
         r = requests.get(uri)
-        output = json.dumps(r.json())
-        return output
+        return r.text
 
     if command == 'graphql':
         headers = {'Authorization': 'token %s' % os.environ['API_TOKEN']}
         r = requests.post('https://api.github.com/graphql', json.dumps({"query": query}), headers=headers)
-        output = json.dumps(r.json())
-        return output
+        return r.text
 
 if __name__ == '__main__':
     print(main(sys.argv[1], sys.argv[2]))
