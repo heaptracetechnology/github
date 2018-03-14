1f10b9b331319c55e19b2f6ca1a149bcd5864598
Josh Pollara <joshpollara@gmail.com>
2018-03-14 10:13:49 +0100

    argument mods

--- github.py
+++ github.py
@@ -4,15 +4,14 @@ import sys
 import requests
 import json
 
-def main(command, request):
+def main(command, query):
     if command == 'get':
-        uri = "https://api.github.com%s" % (request)
+        uri = "https://api.github.com%s" % (query)
         r = requests.get(uri)
         output = json.dumps(r.json())
         return output
 
-    if command == 'query':
-        query = '{ user(login: "joshpollara") { repositories(first: 10) { nodes { languages(first: 3) { nodes { name } } } } } }'
+    if command == 'graphql':
         headers = {'Authorization': 'token %s' % os.environ['API_TOKEN']}
         r = requests.post('https://api.github.com/graphql', json.dumps({"query": query}), headers=headers)
         output = json.dumps(r.json())
