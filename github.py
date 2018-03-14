8ef7171eb572841d442aeefc90b20ef0a99eeb42
Josh Pollara <joshpollara@gmail.com>
2018-03-14 10:09:19 +0100

    support for graphql

--- github.py
+++ github.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
+import os
 import sys
 import requests
 import json
 
-def main(command, query):
+def main(command, request):
     if command == 'get':
-        uri = "https://api.github.com%s" % (query)
+        uri = "https://api.github.com%s" % (request)
         r = requests.get(uri)
+        output = json.dumps(r.json())
+        return output
+
+    if command == 'query':
+        query = '{ user(login: "joshpollara") { repositories(first: 10) { nodes { languages(first: 3) { nodes { name } } } } } }'
+        headers = {'Authorization': 'token %s' % os.environ['API_TOKEN']}
+        r = requests.post('https://api.github.com/graphql', json.dumps({"query": query}), headers=headers)
         output = json.dumps(r.json())
         return output
 
